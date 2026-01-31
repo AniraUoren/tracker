03e3a8f2eb3408f2de4d902ac89a84d4c57220db
Irina Naumova <airin6666@yandex.ru>
2026-01-31 20:02:34 +0300

    feat: TRACKER-1 Добавление возможности редактировать таску

--- my-app/src/components/task/task.jsx
+++ my-app/src/components/task/task.jsx
@@ -1,10 +1,39 @@
 import "./task.css";
+
 import {useState} from "react";
+import "../../assets/edit.svg";
 
 function Task({text}) {
     const [taskText, setTaskText] = useState(text);
+    const [isEditFieldVisible, setIsEditFieldVisible] = useState(false);
+
+    function editTack(taskText) {
+        setIsEditFieldVisible(true);
+    }
+
+    function handleKeyPress(evt) {
+        if (evt.key === 'Enter') {
+            setIsEditFieldVisible(false);
+        }
+    }
+
+    function handleConfirmBtn() {
+        setIsEditFieldVisible(false)
+    }
+
     return (
-        <p className="task">{taskText}</p>
+        <div className={"taskContainer"}>
+            <div className="taskContainer">
+                <p className={isEditFieldVisible !== true ? "task" : "task task_enable"}>{taskText}</p>
+                <input type="text"
+                       value={taskText}
+                       onChange={evt => setTaskText(evt.target.value)}
+                       onKeyDown={handleKeyPress}
+                       className={isEditFieldVisible !== true ? "editTaskField" : "editTaskField editTaskField_enable"}/>
+            </div>
+            <button type="button" className={isEditFieldVisible !== true ? "editButton" : "editButton editButton_disable"} onClick={editTack}>Изменить</button>
+            <button type="button" className={isEditFieldVisible !== true ? "confirmButton" : "confirmButton confirmButton_enable"} onClick={handleConfirmBtn}></button>
+        </div>
     );
 }
 
